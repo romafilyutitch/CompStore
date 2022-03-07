18dcec2f107acb926a8c41e039f2ca503217f9dd
Roman Filiyutich <roman.filiyutich@sap.com>
2022-03-08 01:20:56 +0300

    Initial model, repository, controller for Computer model added

--- src/main/java/com/bsac/CompStore/repository/ComputerRepository.java
+++ src/main/java/com/bsac/CompStore/repository/ComputerRepository.java
@@ -1,9 +1,10 @@
 package com.bsac.CompStore.repository;
 
 import com.bsac.CompStore.model.Computer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public interface ComputerRepository extends CrudRepository<Computer, Integer> {
+public interface ComputerRepository extends JpaRepository<Computer, Integer> {
 }
